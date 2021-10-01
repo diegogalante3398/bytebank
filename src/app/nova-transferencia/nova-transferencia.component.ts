9902e0713881c4d2fca5112e9ebea969505cf1a4
Diego Galante <diego.galante2010@hotmail.com>
2021-09-30 23:02:50 -0300

    Primeiro commit Bytebank

---
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss'],
+})
+export class NovaTransferenciaComponent {
+  valor!: number;
+  destino!: number;
+
+  transferir() {
+    console.log("Valor: ",this.valor);
+    console.log("Destino: ",this.destino);
+  }
+}
